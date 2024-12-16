cf9c293cc0cfe7acca7e5ebbf10a7ec9262cf278
glaceJD <124408179+glaceJD@users.noreply.github.com>
2024-12-15 22:41:31 -0500

    implemented level loading into Client.cpp

--- NetworkingFinal/Client.cpp
+++ NetworkingFinal/Client.cpp
@@ -4,11 +4,21 @@
 #include <string>
 #include "ClientNetworkManager.h"
 
-//Name Matteo Mastrantoni, Student Number N01541856 
-// Job: Client.cpp code 
+/*
+Contributions:
+Name: Matteo Mastrantoni, Student Number N01541856 || Client.cpp code
+Name: Joseph D'Elia, n01438727 || implementing the level/player data into Client.cpp 
+*/
 
 const int WINDOW_WIDTH = 1024;
 const int WINDOW_HEIGHT = 768; 
+
+//game colors
+const SDL_Color PLAYER_COLOR = { 0, 255, 0, 255 };
+const SDL_Color WALL_COLOR = { 255, 255, 255, 255 };
+
+void RunGame(SDL_Renderer* renderer, TCPsocket clientSocket, int playerNumber);
+void HandleGameCommunication(TCPsocket clientSocket, bool& isRunning, std::vector<std::string>& serverMessages);
 
 int main(int argc, char* argv[])
 {
@@ -131,12 +141,88 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	RunGame(renderer, clientSocket, NumberOfPlayer);
+
 	SDLNet_TCP_Close(clientSocket);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDLNet_Quit();
 	SDL_Quit();
 	return 0;
+}
+
+//main game loop
+void RunGame(SDL_Renderer* renderer, TCPsocket clientSocket, int playerNumber)
+{
+	bool isRunning = true;
+	SDL_Event event;
+	Uint32 lastTick = SDL_GetTicks();
+	std::vector<std::string> serverMessages;
+
+	while (isRunning)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				isRunning = false;
+			}
+		}
+
+
+		//clear screen
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		//render player
+		SDL_Rect playerRect = { 100 + (playerNumber * 50), 300, 50, 50 };
+		SDL_SetRenderDrawColor(renderer, PLAYER_COLOR.r, PLAYER_COLOR.g, PLAYER_COLOR.b, PLAYER_COLOR.a);
+		SDL_RenderFillRect(renderer, &playerRect);
+
+		//render floor
+		SDL_Rect floorRect = { 0, WINDOW_HEIGHT - 50, WINDOW_WIDTH, 50 };
+		SDL_SetRenderDrawColor(renderer, WALL_COLOR.r, WALL_COLOR.g, WALL_COLOR.b, WALL_COLOR.a);
+		SDL_RenderFillRect(renderer, &floorRect);
+
+		//present the rendered frame
+		SDL_RenderPresent(renderer);
+
+		//handle server communication
+		HandleGameCommunication(clientSocket, isRunning, serverMessages);
+
+		//frame delay
+		Uint32 currentTick = SDL_GetTicks();
+		if (currentTick - lastTick < 16)
+		{
+			SDL_Delay(16 - (currentTick - lastTick));
+		}
+		lastTick = currentTick;
+
+	}
+}
+
+//handle communication with the server
+void HandleGameCommunication(TCPsocket clientSocket, bool& isRunning, std::vector<std::string>& serverMessages)
+{
+	const int BUFFER_SIZE = 512;
+	char buffer[BUFFER_SIZE] = { 0 };
+
+	//receive server messages
+	int received = SDLNet_TCP_Recv(clientSocket, buffer, BUFFER_SIZE - 1);
+	if (received > 0)
+	{
+		buffer[received] = '\0';
+		serverMessages.push_back(std::string(buffer));
+		std::cout << "Server: " << buffer << std::endl;
+	}
+	else if (received <= 0)
+	{
+		std::cerr << "Disconnected from server." << std::endl;
+		isRunning = false;
+	}
+	
+	std::string positionMessage = "Player" + std::to_string(SDL_GetTicks() % 100);
+	SDLNet_TCP_Send(clientSocket, positionMessage.c_str(), positionMessage.length());
 }
 
 	// on startup, connect to lobby via TCP
